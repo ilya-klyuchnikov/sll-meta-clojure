3f4e3fa85fba39304c2a49ed88351e826a5c446c
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-12-29 13:31:04 +0000

    upgrade

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject sll "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-cloverage "1.0.9"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [[lein-cloverage "1.2.1"]])
